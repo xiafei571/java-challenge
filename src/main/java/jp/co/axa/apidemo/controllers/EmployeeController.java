80d2f094eb68e2e991ee8fba0c8f7b40f36261eb
XIA FEI <xiafei571@gmail.com>
2023-02-26 15:14:03 +0900

    Added some API desc to REST methods

--- src/main/java/jp/co/axa/apidemo/controllers/EmployeeController.java
+++ src/main/java/jp/co/axa/apidemo/controllers/EmployeeController.java
@@ -3,9 +3,11 @@ package jp.co.axa.apidemo.controllers;
 import jp.co.axa.apidemo.entities.Employee;
 import jp.co.axa.apidemo.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import io.swagger.annotations.ApiOperation;
+
 
 @RestController
 @RequestMapping("/api/v1")
@@ -18,29 +20,35 @@ public class EmployeeController {
         this.employeeService = employeeService;
     }
 
+    @ApiOperation(value = "Get the employee list by page",
+    		notes = "pageIndex start from 1, pageSize shoud be greater than 0")
     @GetMapping("/employees")
-    public List<Employee> getEmployees() {
-        List<Employee> employees = employeeService.retrieveEmployees();
+    public Page<Employee> getEmployees(@RequestParam(required = false) Integer pageIndex, @RequestParam(required = false) Integer pageSize) {
+    	Page<Employee> employees = employeeService.retrieveEmployees(pageIndex, pageSize);
         return employees;
     }
-
+    
+    @ApiOperation(value = "Get the employee info by ID")
     @GetMapping("/employees/{employeeId}")
     public Employee getEmployee(@PathVariable(name="employeeId")Long employeeId) {
         return employeeService.getEmployee(employeeId);
     }
-
+    
+    @ApiOperation(value = "Add a new employee info")
     @PostMapping("/employees")
     public void saveEmployee(Employee employee){
         employeeService.saveEmployee(employee);
         System.out.println("Employee Saved Successfully");
     }
 
+    @ApiOperation(value = "Delete a employee info")
     @DeleteMapping("/employees/{employeeId}")
     public void deleteEmployee(@PathVariable(name="employeeId")Long employeeId){
         employeeService.deleteEmployee(employeeId);
         System.out.println("Employee Deleted Successfully");
     }
 
+    @ApiOperation(value = "Update a employee info")
     @PutMapping("/employees/{employeeId}")
     public void updateEmployee(@RequestBody Employee employee,
                                @PathVariable(name="employeeId")Long employeeId){
