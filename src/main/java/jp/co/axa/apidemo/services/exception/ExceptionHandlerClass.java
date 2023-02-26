2996613550dbf20bd378197fc7b3f4bceadc5f11
XIA FEI <xiafei571@gmail.com>
2023-02-26 15:15:06 +0900

    Added response exception handling.

---
+++ src/main/java/jp/co/axa/apidemo/services/exception/ExceptionHandlerClass.java
@@ -0,0 +1,36 @@
+package jp.co.axa.apidemo.services.exception;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import jp.co.axa.apidemo.services.response.Result;
+
+@ControllerAdvice
+@ResponseBody
+@SuppressWarnings("rawtypes")
+public class ExceptionHandlerClass {
+
+	/**
+	 * All unspecified exceptions will be defaulted to SYSTEM_ERROR status
+	 * 
+	 * @return
+	 */
+	@ExceptionHandler(Exception.class)
+	public Result ExceptionHandler() {
+		return new Result<>(ErrorCodeEnum.SYSTEM_ERROR);
+	}
+
+	/**
+	 * If a value is not present, return RES_NOT_EXIST status
+	 * 
+	 * @return
+	 */
+	@ExceptionHandler(NoSuchElementException.class)
+	public Result ResNotExistHandler() {
+		return new Result<>(ErrorCodeEnum.RES_NOT_EXIST);
+	}
+
+}
